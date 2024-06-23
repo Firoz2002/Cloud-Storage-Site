60ae63de4161dbb705efd4f2490772af47083365
FirozKamdar <105146975+Firoz2002@users.noreply.github.com>
2024-06-23 21:37:41 +0530

    Updated config

--- Server/config/config.js
+++ Server/config/config.js
@@ -2,25 +2,15 @@ const { initializeApp } = require('firebase/app');
 const { getFirestore } = require('firebase/firestore');
 
 require('dotenv').config();
-/*
+
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
+    projectId: "storage-site-9c81e" /*process.env.PROJECT_ID*/,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
     measurementId: process.env.MESAUREMENT_ID
-}*/
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCH8tuZNbaeS8PliXego41qrkZoGwMX63Q",
-    authDomain: "storage-site-9c81e.firebaseapp.com",
-    projectId: "storage-site-9c81e",
-    storageBucket: "storage-site-9c81e.appspot.com",
-    messagingSenderId: "875022356094",
-    appId: "1:875022356094:web:5c1c95c6094f335895922f",
-    measurementId: "G-7MEBXEQ4FH"
 };
 
 const app = initializeApp(firebaseConfig);
